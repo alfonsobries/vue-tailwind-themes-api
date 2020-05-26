6e849a05e1c9a332680d95d159fb0b6107f63fea
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-05-26 09:54:50 -0500

    Add intiials to user

--- app/User.php
+++ app/User.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use Illuminate\Support\Arr;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'initials',
+        'just_name',
+    ];
+
+    /**
+     * User initials
+     *
+     * @return string
+     */
+    public function getInitialsAttribute()
+    {
+        preg_match_all('#(?<=\s|\b)\pL#u', $this->name, $initials);
+
+        return strtoupper(implode('', array_slice($initials[0], 0, 2)));
+    }
+
+    /**
+     * User name
+     *
+     * @return string
+     */
+    public function getJustNameAttribute()
+    {
+        $pieces = explode(' ', $this->name);
+
+        return trim(Arr::get($pieces, 0));
+    }
 }
